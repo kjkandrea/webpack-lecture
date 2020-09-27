4cd21f302744a2c86f98131eb3626eb416207cc6
Jongkeun Kim <kjkandrea@gmail.com>
2020-09-27 14:53:32 +0900

    webpack plugin으로 빌드 결과물에 문자열(주석) 삽입해보기

--- my-webpack-plugin.js
+++ my-webpack-plugin.js
@@ -1,7 +1,23 @@
 class MyWebpackPlugin {
-  apply(complier) {
-    complier.hooks.done.tap('My Webpack Plugin', stats => {
+  apply(compiler) {
+    compiler.hooks.done.tap('My Webpack Plugin', stats => {
       console.log('My Webpack Plugin : done')
+    })
+
+    compiler.plugin('emit', (compilation, callback) => {
+      const source = compilation.assets['main.js'].source();
+
+      compilation.assets['main.js'].source = () => {
+        const banner = [
+          '/**',
+          ' * 이것은 BannerPlugin이 처리한 결과입니다.',
+          ' * Build Date: 2019-10-10',
+          ' */'
+        ].join('\n');
+        return banner + '\n' + source;
+      }
+
+      callback();
     })
   }
 }
