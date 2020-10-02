b31d5c3df89f02efc8ca49efe33363c97b9edbbe
Jongkeun Kim <kjkandrea@gmail.com>
2020-10-02 18:24:11 +0900

    webpack-dev-server mockup api #2

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import * as math from "./utils/mathUtil.js";
 import "./app.css";
 import nyancat from "./nyancat.jpg";
+import axios from "axios";
 
 console.log(math.sum(2, 3));
 console.log(process.env.NODE_ENV);
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const preEl = document.createElement("pre");
   preEl.textContent = `
     빌드 환경 : ${process.env.NODE_ENV}
@@ -16,4 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const imageEl = document.createElement("img");
   imageEl.src = nyancat;
   document.body.append(imageEl);
+
+  const { users } = await axios.get("/api/users");
+  console.log(users);
 });
